f4b8168e1f7abbc8b6d9cbfa34b70f0a1d16ca31
Pavel198489 <152794772+Pavel198489@users.noreply.github.com>
2024-02-27 21:31:20 +0500

    28.02.2024

--- Блокировки потоков для доступа к общим данным.py
+++ Блокировки потоков для доступа к общим данным.py
@@ -62,8 +62,8 @@ th_withdraw.join()
 # Deposited 100, new balance is 1300
 # Deposited 100, new balance is 1400
 # Deposited 100, new balance is 1500
-# Withdrew 150,  new balance is  1350
-# Withdrew 150,  new balance is  1200
-# Withdrew 150,  new balance is  1050
-# Withdrew 150,  new balance is  900
-# Withdrew 150,  new balance is  750
+# Withdrew 150,  new balance is 1350
+# Withdrew 150,  new balance is 1200
+# Withdrew 150,  new balance is 1050
+# Withdrew 150,  new balance is 900
+# Withdrew 150,  new balance is 750
