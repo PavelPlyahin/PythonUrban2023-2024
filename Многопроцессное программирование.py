c08c82767542abf0bb02d36eaaade5e26f1a5d81
Pavel198489 <152794772+Pavel198489@users.noreply.github.com>
2024-03-06 23:22:19 +0500

    07.03.2024

--- Многопроцессное программирование.py
+++ Многопроцессное программирование.py
@@ -8,19 +8,20 @@ class WarehouseManager:
     def process_request(self, request):
         product, action, quantity = request
         if product in self.data:
-            if action == 'receipt':
+            if action == "receipt":
                 self.data[product] += quantity
-            elif action == 'shipment':
+            elif action == "shipment":
                 if quantity < self.data[product]:
                     print('нет требуемого количества на складе')
                 self.data[product] -= quantity
         else:
             self.data[product] = quantity
 
+
     def run(self, requests):
         processes = []
         for request in requests:
-            process.append(multiprocessing.Process(target=self.process_request, args=(request,)))
+            processes.append(multiprocessing.Process(target=self.process_request, args=(request,)))
         for i in processes:
             i.start()
         for i in processes:
@@ -37,4 +38,5 @@ if __name__ == '__main__':
         ("product3", "receipt", 200),
         ("product2", "shipment", 50)
     ]
+    manager.run(requests)
     print(manager.data)
