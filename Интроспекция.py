96e3521b3f2e593e4ff50c419a7d43a45c379b34
plyahin <pavelplyahin@yandex.ru>
2024-03-14 16:47:02 +0500

    14.03.2024

--- Интроспекция.py
+++ Интроспекция.py
@@ -6,23 +6,39 @@
 #    - Методы объекта.
 #    - Модуль, к которому объект принадлежит.
 #    - Другие интересные свойства объекта, учитывая его тип (по желанию).
+class IntrospectionHelper:
+    def __init__(self, obj):
+        self.obj = obj
 
-import types
+    def get_type(self):
+        return type(self.obj)
 
+    def get_attributes(self):
+        return dir(self.obj)
+
+    def get_methods(self):
+        methods = []
+        for attr in self.get_attributes():
+            if attr[0] != '_' and callable(getattr(self.obj, attr)):
+                methods.append(attr)
+        return methods
+
+    def get_module(self):
+        module = self.obj.__class__.__module__
+        if module == 'builtins':
+            module = 'None'
+        return module
 
 def introspection_info(obj):
-    info = {}
-    module = obj.__class__.__module__
-    class_name = obj.__class__.__name__
-    attributes = obj.__dict__.keys()
-    methods = dir(obj)
-
-    info['Type'] = class_name
-    info['Module'] = module
-    info['Attributes'] = attributes
-    info['Methods'] = methods
-
+    helper = IntrospectionHelper(obj)
+    info = {
+        'type': helper.get_type(),
+        'attributes': helper.get_attributes(),
+        'methods': helper.get_methods(),
+        'module': helper.get_module()
+    }
     return info
 
-number_info = introspection_info(42)
+
+number_info = introspection_info('Привет буфет 1984 ')
 print(number_info)
